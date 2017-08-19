c27fee6e96511cde42d6c6cf22a2a138cbd281e6
Fredrik Brännbacka FL <fredrik.brannbacka@ilpvfx.com>
2017-08-19 22:57:35 +0200

    Build both static and shared, prepend library path.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ variants = [
 uuid = "rezpo.boost"
 
 def commands():
-    env.LD_LIBRARY_PATH.append("{root}/lib")
+    env.LD_LIBRARY_PATH.prepend("{root}/lib")
     env.BOOST_ROOT = '{root}'
     if building:
         env.CMAKE_MODULE_PATH.append("{root}/cmake")
