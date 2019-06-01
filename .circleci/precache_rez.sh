66b292fab595ad7263d459bd0eff8c9dcf36f1b0
Fredrik Brännbacka <fredrik.brannbacka@gmail.com>
2019-06-01 19:25:13 +0200

    testing circleci

--- .circleci/precache_rez.sh
+++ .circleci/precache_rez.sh
@@ -6,10 +6,12 @@ cd rez
 python ./install.py -v $HOME/rez
 cd ..
 mkdir -p $HOME/packages
-rez-bind platform
-rez-bind arch
-rez-bind os
-rez-bind python
+ls -al $HOME/rez/bin/
+ls -al $HOME/rez/bin/rez/
+$HOME/rez/bin/rez/rez-bind platform
+$HOME/rez/bin/rez/rez-bind arch
+$HOME/rez/bin/rez/rez-bind os
+$HOME/rez/bin/rez/rez-bind python
 git clone https://github.com/piratecrew/rez-python.git
 cd rez-python
 $HOME/rez/bin/rez/rez-build -i
