1e4f11d599c763a29b2c149d6459b09dec4d175b
Fredrik Brännbacka <fredrik.brannbacka@gmail.com>
2019-06-01 18:09:46 +0200

    fixing travis caching

---
+++ .travis/precache_rez.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ -d "$HOME/rez/bin/rez"]; then
+    echo "Cache retrieved!"
+    exit 0
+fi
+OLDPWD=$PWD
+cd /tmp
+git clone https://github.com/nerdvegas/rez.git
+cd rez
+python ./install.py -v $HOME/rez
+cd ..
+mkdir -p $HOME/packages
+rez-bind platform
+rez-bind arch
+rez-bind os
+rez-bind python
+git clone https://github.com/piratecrew/rez-python.git
+cd rez-python
+$HOME/rez/bin/rez/rez-build -i
+cd $PWD
