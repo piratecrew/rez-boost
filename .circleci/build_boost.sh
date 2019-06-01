96a6a830c402cb5ab696170800eeee2d7c86c340
Fredrik Brännbacka <fredrik.brannbacka@gmail.com>
2019-06-01 19:01:09 +0200

    testing circleci

---
+++ .circleci/build_boost.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -pv ${REZ_REPO_PAYLOAD_DIR}
+echo "CREATED REZ_REPO_PAYLOAD_DIR ${REZ_REPO_PAYLOAD_DIR}"
+BOOST_ARCHIVE_URL=http://downloads.sourceforge.net/project/boost/boost/${BOOST_VERSION}/boost_$(echo ${BOOST_VERSION} | tr "." "_").tar.gz
+echo "BOOST ARCHIVE URL ${BOOST_ARCHIVE_URL}"
+OLDPWD=$PWD
+mkdir -p ${REZ_REPO_PAYLOAD_DIR}/boost
+cd ${REZ_REPO_PAYLOAD_DIR}/boost
+curl -LO $BOOST_ARCHIVE_URL
+cd $OLDPWD
+sed -i s/version.*=.*/version=\"${BOOST_VERSION}\"/ package.py
+$HOME/rez/bin/rez/rez-build -i
